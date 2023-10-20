4053b56aaaa30ade14010e8118e930d774212de0
OJ Laboeuf <orangepeeelbeef@users.noreply.github.com>
2023-10-19 22:11:04 -0700

    add some more console logs

--- scripts/xp.js
+++ scripts/xp.js
@@ -8,6 +8,7 @@ Hooks.once("init", function() {
 Hooks.on("renderPartySheetPF2e", async (actor_directory, html, data) => {
     if (!game.user.isGM)
         return
+    console.log("Attempting to render pf2e-award-xp input box")
     const awardButton = $(`<label for="pf2e-award-xp-value">XP:<input type="text" id="pf2e-award-xp-value" name="pf2e-award-xp-value"><button>Grant XP</button>`)
     html.find(".sheet.party [data-tab=overview] .summary").append((awardButton))
     awardButton.click((event) => {award_xp()})
@@ -17,6 +18,7 @@ Hooks.on("renderPartySheetPF2e", async (actor_directory, html, data) => {
 function award_xp() {
     const xp_to_award = parseInt(html.querySelector("#pf2e-award-xp-value").value)
     const party_members = game.actors.party.members
+    console.log("Attempting to add XP to party members")
     party_members.forEach(pc => {
         pc.new_xp = pc.xp + xp_to_award
         const update_data = {}
